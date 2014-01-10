bbfff395dd2e17594a3a4537728a38fb36773e91
Jeff Weiss <jweiss@redhat.com>
2014-01-10 09:08:59 -0500

    Move to package directory

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="ui_navigate",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     #scripts=['say_hello.py'],
 
